ae35a6747670bd599913614ba34a955201ea59b2
David Erni <dave.erni@gmail.com>
2018-03-18 13:09:06 +0100

    Add PayloadSeparatorPart example part.

---
+++ PayloadPart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BeenThereDoneThat
+{
+    [KSPAddon(KSPAddon.Startup.FlightAndEditor, false)]
+    public class PayloadSeparatorPart : PartModule
+    {
+        [KSPField(isPersistant = true, guiActive = true, guiActiveEditor = true)]
+        public bool isPayloadNode;
+    }
+}
