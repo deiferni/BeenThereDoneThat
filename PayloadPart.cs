efd04522c776b2f979c77b913929bb10087ad5fa
David Erni <dave.erni@gmail.com>
2018-03-18 16:35:04 +0100

    Put ship into orbit of current body.

--- PayloadPart.cs
+++ PayloadPart.cs
@@ -7,5 +7,29 @@ namespace BeenThereDoneThat
     {
         [KSPField(isPersistant = true, guiActive = true, guiActiveEditor = true)]
         public bool isPayloadNode;
+
+        [KSPEvent(guiActive = true, guiName = "Put me into orbit. NOW!")]
+        public void PutMeIntoOrbitNow()
+        {
+            ScreenMessages.PostScreenMessage(
+                "Putting the damn thing into orbit.",
+                5.0f,
+                ScreenMessageStyle.UPPER_CENTER);
+
+            CelestialBody body = FlightGlobals.ActiveVessel.mainBody;
+            int selBodyIndex = FlightGlobals.Bodies.IndexOf(body);
+            double sma = body.minOrbitalDistance * 1.1;
+
+            double ecc = 0;
+            double inc = 0;
+            double LAN = 0;
+            double mna = 0;
+            double argPe = 0;
+            double ObT = 0;
+
+            Debug.Log(string.Format("Setting 'sma' to {0} based on minimal distance {1}", sma, body.minOrbitalDistance));
+
+            FlightGlobals.fetch.SetShipOrbit(selBodyIndex, ecc, sma, inc, LAN, mna, argPe, ObT);
+        }
     }
 }
