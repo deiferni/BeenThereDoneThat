ee1ca7c4c3a8c1d5981cb5c4eccad620777c5796
David Erni <david.erni@4teamwork.ch>
2018-03-27 22:42:27 +0200

    Add draggable example window.
    Phew. The unity docs are the thing to look at. Pretty
    straight forward for now once that was figured out.

--- BeenthereDoneThatDisplay.cs
+++ BeenthereDoneThatDisplay.cs
@@ -10,6 +10,17 @@ namespace BeenThereDoneThat
         static ApplicationLauncherButton beenThereDoneThatButton = null;
         static ApplicationLauncher.AppScenes buttonScenes = ApplicationLauncher.AppScenes.VAB | ApplicationLauncher.AppScenes.SPH | ApplicationLauncher.AppScenes.FLIGHT;
 
+        private GUIStyle windowStyle;
+        private GUIStyle labelStyle;
+        private GUIStyle labelTextStyle;
+        private GUIStyle textFieldStyle;
+        private GUIStyle buttonStyle;
+        private GUIStyle horizontalScrollBarStyle;
+        private GUIStyle verticalScrollBarStyle;
+        private bool initializedStyles = false;
+
+        private static Rect windowPos = new Rect(500, 400, 300, 75);
+
         public void Update()
         {
             if (!ApplicationLauncher.Ready || buttonAdded)
@@ -41,6 +52,66 @@ namespace BeenThereDoneThat
                 buttonAdded = true;
             }
             return button;
+        }
+
+        void OnGUI()
+        {
+            initStyle();
+            windowPos = GUILayout.Window(9741, windowPos, OnWindow, "My window", windowStyle);
+        }
+
+        public void OnWindow(int windowID)
+        {
+            GUILayout.BeginVertical();
+            GUILayout.BeginHorizontal();
+
+            GUILayout.Label("Hello KSP", labelStyle);
+            GUILayout.Button("Klick me", buttonStyle);
+
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
+
+            GUILayout.Label("Ohh yeahh", labelStyle);
+            GUILayout.Button("Klickkkkk meeeee!", buttonStyle);
+
+            GUILayout.EndHorizontal();
+            GUILayout.EndVertical();
+
+            GUI.DragWindow();
+        }
+
+        private void initStyle()
+        {
+            if (initializedStyles)
+            {
+                return;
+            }
+            windowStyle = new GUIStyle(HighLogic.Skin.window);
+            windowStyle.stretchWidth = false;
+            windowStyle.stretchHeight = false;
+
+            labelStyle = new GUIStyle(HighLogic.Skin.label);
+            labelStyle.stretchWidth = false;
+            labelStyle.stretchHeight = false;
+
+            labelTextStyle = new GUIStyle(HighLogic.Skin.label);
+            labelTextStyle.stretchWidth = false;
+            labelTextStyle.stretchHeight = true;
+            labelTextStyle.wordWrap = true;
+
+            textFieldStyle = new GUIStyle(HighLogic.Skin.textField);
+            textFieldStyle.stretchWidth = false;
+            textFieldStyle.stretchHeight = false;
+
+            buttonStyle = new GUIStyle(HighLogic.Skin.button);
+            buttonStyle.stretchHeight = false;
+            buttonStyle.stretchWidth = false;
+
+            horizontalScrollBarStyle = new GUIStyle(HighLogic.Skin.horizontalScrollbar);
+            verticalScrollBarStyle = new GUIStyle(HighLogic.Skin.verticalScrollbar);
+
+            initializedStyles = true;
         }
 
         public void OnTrue()
