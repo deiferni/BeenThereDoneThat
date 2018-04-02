99e29af5c02c25579a05f6db9f4218e29afd34b2
David Erni <david.erni@4teamwork.ch>
2018-04-02 22:31:00 +0200

    Attempt to extract dialog into own class.

--- QuickLaunchHangar.cs
+++ QuickLaunchHangar.cs
@@ -6,21 +6,10 @@ namespace BeenThereDoneThat
     [KSPAddon(KSPAddon.Startup.FlightAndEditor, false)]
     public class QuickLaunchHangar : MonoBehaviour
     {
-
-        private enum FilenameCheckResults
-        {
-            Empty,
-            AlreadyExists,
-            InvalidCharacters,
-            Good
-        }
-
         public static string ORBITFILENAME = "VesselOrbit.craft";
         public static string LAUNCHFILENAME = "VesselLaunch.craft";
 
-        private PopupDialog confirmDialog;
-        private PopupDialog saveDialog;
-        private string saveFilename;
+        private SaveMissionDialog saveMissionDialog;
 
         public static QuickLaunchHangar Instance
         {
@@ -33,6 +22,11 @@ namespace BeenThereDoneThat
             Instance = this;
         }
 
+        public void OnSaveMissionDialogDismiss()
+        {
+            saveMissionDialog = null;
+        }
+
         public void SaveOrbitVessel(Vessel vessel)
         {
             if (vessel.situation != Vessel.Situations.ORBITING)
@@ -40,87 +34,8 @@ namespace BeenThereDoneThat
                 ScreenMessages.PostScreenMessage("Can't save yet, must be orbiting!", 4, ScreenMessageStyle.UPPER_CENTER);
                 return;
             }
-            Orbit orbit = vessel.orbit;
-            CelestialBody body = vessel.mainBody;
 
-
-            this.saveFilename = "VesselOrbit";
-            MultiOptionDialog dialog = new MultiOptionDialog("Save mission", "Save mission", "Save mission", UISkinManager.GetSkin("MainMenuSkin"),
-                            new DialogGUITextInput(this.saveFilename, false, 64, delegate (string name)
-                            {
-                                this.saveFilename = name;
-                                return this.saveFilename;
-                            }, 24f), new DialogGUIButton("Save", delegate
-                            {
-                                this.ConfirmDialog(vessel);
-                            }, false), new DialogGUIButton("Cancel", delegate
-                            {
-                                this.onSaveDialogDismiss();
-                            }, true));
-            this.saveDialog = PopupDialog.SpawnPopupDialog(new Vector2(0.5f, 0.5f), new Vector2(0.5f, 0.5f), dialog, false, null, true, string.Empty);
-
-            int selBodyIndex = FlightGlobals.Bodies.IndexOf(body);
-            double sma = orbit.semiMajorAxis;
-            double ecc = orbit.eccentricity;
-            double inc = orbit.inclination;
-            double LAN = orbit.LAN;
-            double mna = orbit.meanAnomalyAtEpoch;
-            double argPe = orbit.argumentOfPeriapsis;
-
-            Debug.Log(
-                string.Format("[BeenThereDoneThat]: REMEMBERING ORBIT> sma: {0} ecc: {1} inc: {2} LAN: {3} mna: {4} argPe: {5}",
-                              sma, ecc, inc, LAN, mna, argPe));
-        }
-
-        public void ConfirmDialog(Vessel vessel)
-        {
-            string filename = this.saveFilename + ".craft";
-            string path = MakeHangarPath(filename);
-            if (File.Exists(path))
-            {
-                this.saveDialog.gameObject.SetActive(false);
-                this.confirmDialog = PopupDialog.SpawnPopupDialog(new Vector2(0.5f, 0.5f), new Vector2(0.5f, 0.5f), new MultiOptionDialog("SavegameConfirmation", "Overwrite " + this.saveFilename, "Overwrite", UISkinManager.GetSkin("MainMenuSkin"), new DialogGUIButton("Overwrite", delegate
-                {
-                    SaveVessel(vessel, filename);
-                    this.confirmDialog.Dismiss();
-                    this.onSaveDialogDismiss();
-                }, true), new DialogGUIButton("Cancel", delegate
-                {
-                    this.confirmDialog.Dismiss();
-                    this.saveDialog.gameObject.SetActive(true);
-                }, true)), false, null, true, string.Empty);
-                return;
-            }
-            if (!this.CheckFilename(this.saveFilename))
-            {
-                this.confirmDialog = PopupDialog.SpawnPopupDialog(new Vector2(0.5f, 0.5f), new Vector2(0.5f, 0.5f), new MultiOptionDialog("SavegameInvalidName", "Oh crap", "Oh crap", UISkinManager.GetSkin("MainMenuSkin"), new DialogGUIButton("Oh crap", delegate
-                {
-                    this.confirmDialog.Dismiss();
-                    this.saveDialog.gameObject.SetActive(true);
-                }, true)), false, null, true, string.Empty);
-                return;
-            }
-
-            SaveVessel(vessel, filename);
-            onSaveDialogDismiss();
-        }
-
-        private bool CheckFilename(string filename)
-        {
-            if (string.IsNullOrEmpty(filename))
-            {
-                return false;
-            }
-            if (KSPUtil.SanitizeFilename(filename) != filename)
-            {
-                return false;
-            }
-            return true;
-        }
-
-        public void onSaveDialogDismiss()
-        {
-            this.saveDialog.Dismiss();
+            saveMissionDialog = SaveMissionDialog.Create(OnSaveMissionDialogDismiss, vessel);
         }
 
         public static ProtoVessel LoadOrbitProtoVessel()
@@ -140,21 +55,21 @@ namespace BeenThereDoneThat
             SaveVessel(vessel, LAUNCHFILENAME);
         }
 
-        protected static void SaveVessel(Vessel vessel, string filename)
+        public static void SaveVessel(Vessel vessel, string filename)
         {
             ConfigNode node = new ConfigNode();
             vessel.protoVessel.Save(node);
             node.Save(MakeHangarPath(filename));
         }
 
-        protected static string MakeHangarDirectory()
+        public static string MakeHangarDirectory()
         {
             string directory = KSPUtil.ApplicationRootPath + "saves/" + HighLogic.SaveFolder + "/BeenThereDoneThat/";
             System.IO.Directory.CreateDirectory(directory);
             return directory;
         }
 
-        protected static string MakeHangarPath(string filename)
+        public static string MakeHangarPath(string filename)
         {
             return System.IO.Path.Combine(MakeHangarDirectory(), filename);
         }
---
+++ SaveMissionDialog.cs
@@ -0,0 +1,140 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace BeenThereDoneThat
+{
+    class SaveMissionDialog : MonoBehaviour
+    {
+        private Callback onDismiss;
+        private PopupDialog saveDialog;
+        private PopupDialog confirmDialog;
+
+        private string saveFilename;
+        private Vessel vessel;
+
+        private static Vector2 anchorMin = new Vector2(0.5f, 0.5f);
+        private static Vector2 anchorMax = new Vector2(0.5f, 0.5f);
+
+        public static SaveMissionDialog Instance
+        {
+            get;
+            private set;
+        }
+
+        public static SaveMissionDialog Create(Callback onDismissMenu, Vessel vessel)
+        {
+            GameObject gameObject = new GameObject("BeenThereDoneThat mission save menu");
+            SaveMissionDialog saveMissionDialog = gameObject.AddComponent<SaveMissionDialog>();
+            SaveMissionDialog.Instance = saveMissionDialog;
+
+            saveMissionDialog.onDismiss = onDismissMenu;
+            saveMissionDialog.vessel = vessel;
+
+            saveMissionDialog.ShowSaveDialog();
+            return saveMissionDialog;
+        }
+
+        public void Awake()
+        {
+            Instance = this;
+            FlightDriver.SetPause(true, true);
+            InputLockManager.SetControlLock("BeenThereDoneThat SaveMissionDialog");
+        }
+
+        private void OnDestroy()
+        {
+            if (Instance != null)
+            {
+                if (Instance == this)
+                {
+                    Instance = null;
+                }
+            }
+            InputLockManager.RemoveControlLock("BeenThereDoneThat SaveMissionDialog");
+            FlightDriver.SetPause(false, true);
+        }
+
+        private void Update()
+        {
+            if (saveDialog != null)
+            {
+                return;
+            }
+        }
+
+
+        public void ShowSaveDialog()
+        {          
+            saveFilename = vessel.GetDisplayName();
+            MultiOptionDialog dialog = new MultiOptionDialog("Save mission", "Save mission", "Save mission", UISkinManager.GetSkin("MainMenuSkin"),
+                            new DialogGUITextInput(this.saveFilename, false, 64, delegate (string name)
+                            {
+                                this.saveFilename = name;
+                                return this.saveFilename;
+                            }, 24f), new DialogGUIButton("Save", delegate
+                            {
+                                this.ConfirmDialog();
+                            }, false), new DialogGUIButton("Cancel", delegate
+                            {
+                                this.Dismiss();
+                            }, true));
+            this.saveDialog = PopupDialog.SpawnPopupDialog(anchorMin, anchorMax, dialog, false, null, true, string.Empty);
+        }
+
+        public void ConfirmDialog()
+        {
+            string filename = this.saveFilename + ".craft";
+            string path = QuickLaunchHangar.MakeHangarPath(filename);
+            if (File.Exists(path))
+            {
+                this.saveDialog.gameObject.SetActive(false);
+                this.confirmDialog = PopupDialog.SpawnPopupDialog(anchorMin, anchorMax, new MultiOptionDialog("SavegameConfirmation", "Overwrite " + this.saveFilename, "Overwrite", UISkinManager.GetSkin("MainMenuSkin"), new DialogGUIButton("Overwrite", delegate
+                {
+                    QuickLaunchHangar.SaveVessel(vessel, filename);
+                    this.confirmDialog.Dismiss();
+                    this.Dismiss();
+                }, true), new DialogGUIButton("Cancel", delegate
+                {
+                    this.confirmDialog.Dismiss();
+                    this.saveDialog.gameObject.SetActive(true);
+                }, true)), false, null, true, string.Empty);
+                return;
+            }
+            if (!this.CheckFilename(this.saveFilename))
+            {
+                this.confirmDialog = PopupDialog.SpawnPopupDialog(anchorMin, anchorMax, new MultiOptionDialog("SavegameInvalidName", "Oh crap", "Oh crap", UISkinManager.GetSkin("MainMenuSkin"), new DialogGUIButton("Oh crap", delegate
+                {
+                    this.confirmDialog.Dismiss();
+                    this.saveDialog.gameObject.SetActive(true);
+                }, true)), false, null, true, string.Empty);
+                return;
+            }
+
+            QuickLaunchHangar.SaveVessel(vessel, filename);
+            Dismiss();
+        }
+
+        private bool CheckFilename(string filename)
+        {
+            if (string.IsNullOrEmpty(filename))
+            {
+                return false;
+            }
+            if (KSPUtil.SanitizeFilename(filename) != filename)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void Dismiss()
+        {
+            onDismiss();
+            if (saveDialog != null)
+            {
+                saveDialog.Dismiss();
+            }
+            Destroy(base.gameObject);
+        }
+    }
+}
