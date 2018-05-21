cd9372ac8d83bb68984840b82b0da59ecc5f38fe
David Erni <david.erni@4teamwork.ch>
2018-05-21 16:32:43 +0200

    Use tree to split into LV and payload.
    The previous assumption that the parts are always stored in correct order was
    (as suspected) not always correct. So we re-build the tree for ProtoVessel
    and its ProtoPartSnapshots from parent pointers in the snapshots. I might have
    used AttachNodeSnapshot for this, too. Not sure though. May need to rewrite
    in the future.
    
    For Vessel we use the parent and child pointers of each Part.
    
    For both cases children are ordered according to their part name. That may need
    to be improved in the future for asymmetric builds though.

--- QuickLauncher.cs
+++ QuickLauncher.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace BeenThereDoneThat
@@ -45,23 +46,13 @@ namespace BeenThereDoneThat
                 return false;
             }
 
-            int payloadPartSeparationIndex = payloadSeparator.separationIndex;
-            List<ProtoPartSnapshot> payloadParts = new List<ProtoPartSnapshot>();
-            List<ProtoPartSnapshot> launchVehicleParts = new List<ProtoPartSnapshot>();
-            foreach (ProtoPartSnapshot part in protoVessel.protoPartSnapshots)
-            {
-                if (part.separationIndex >= payloadPartSeparationIndex)
-                {
-                    launchVehicleParts.Add(part);
-                }
-                else
-                {
-                    payloadParts.Add(part);
-                }
-            }
+            ProtoPartSnapshotTree tree = new ProtoPartSnapshotTree(protoVessel.protoPartSnapshots);
 
-            launchVehicle = new ProtoLaunchVehicle(payloadSeparator, launchVehicleParts);
+            List<ProtoPartSnapshot> payloadParts;
+            List<ProtoPartSnapshot> launchVehicleParts;
+            tree.Split(payloadSeparator, out launchVehicleParts, out payloadParts);
             payload = new ProtoPayload(payloadParts);
+            launchVehicle = new ProtoLaunchVehicle(payloadSeparator, launchVehicleParts);
             return true;
         }
 
@@ -76,20 +67,12 @@ namespace BeenThereDoneThat
                 return false;
             }
 
-            int payloadPartSeparationIndex = payloadSeparator.separationIndex;
+            Part root = parts[0].localRoot;
             List<Part> payloadParts = new List<Part>();
             List<Part> launchVehicleParts = new List<Part>();
-            foreach (Part part in parts)
-            {
-                if (part.separationIndex >= payloadPartSeparationIndex)
-                {
-                    launchVehicleParts.Add(part);
-                }
-                else
-                {
-                    payloadParts.Add(part);
-                }
-            }
+
+            PartTreeHelper.AddBreadthFirst(root, payloadParts, payloadSeparator);
+            PartTreeHelper.AddBreadthFirst(payloadSeparator, launchVehicleParts);
 
             launchVehicle = new LaunchVehicle(payloadSeparator, launchVehicleParts);
             payload = new Payload(payloadParts);
@@ -126,6 +109,154 @@ namespace BeenThereDoneThat
                 }
             }
             return null;
+        }
+    }
+
+    internal class PartTreeHelper
+    {
+        class PartNameComparer : IComparer<Part>
+        {
+            public int Compare(Part x, Part y)
+            {
+                return x.name.CompareTo(y.name);
+            }
+        }
+
+        internal static void AddBreadthFirst(Part start, List<Part> parts)
+        {
+            AddBreadthFirst(start, parts, null);
+        }
+
+        internal static void AddBreadthFirst(Part start, List<Part> parts, Part stopAt)
+        {
+            parts.Add(start);
+            List<Part> children = new List<Part>(start.children);
+            children.Sort(new PartNameComparer());
+
+            foreach (Part child in children)
+            {
+                if (child != stopAt)
+                {
+                    AddBreadthFirst(child, parts, stopAt);
+                }
+            }
+        }
+    }
+
+    internal class ProtoPartSnapshotTree
+    {
+        Dictionary<uint, ProtoPartSnapshotNode> tree;
+        ProtoPartSnapshotNode root;
+        List<ProtoPartSnapshot> protoPartSnapshots;
+
+        internal ProtoPartSnapshotTree(List<ProtoPartSnapshot> snapshots)
+        {
+            tree = new Dictionary<uint, ProtoPartSnapshotNode>();
+            protoPartSnapshots = snapshots;
+            root = null;
+
+            foreach (ProtoPartSnapshot part in protoPartSnapshots)
+            {
+                CreateNode(part);
+            }
+
+        }
+
+        internal ProtoPartSnapshotNode CreateNode(ProtoPartSnapshot part)
+        {
+            ProtoPartSnapshotNode node = null;
+            ProtoPartSnapshotNode parentNode = null;
+
+            ProtoPartSnapshot parent = protoPartSnapshots[part.parentIdx];
+            if (parent != part)
+            {
+                parentNode = CreateNode(parent);
+            }
+            if (!tree.TryGetValue(part.persistentId, out node))
+            {
+                node = new ProtoPartSnapshotNode(part, parentNode);
+                tree.Add(part.persistentId, node);
+            }
+            if (parentNode == null)
+            {
+                root = node;
+            }
+            return node;
+        }
+
+        internal void Split(ProtoPartSnapshot separatorPart, out List<ProtoPartSnapshot> launchVehicleParts, out List<ProtoPartSnapshot> payloadParts)
+        {
+            ProtoPartSnapshotNode splitAt = null;
+            if (!tree.TryGetValue(separatorPart.persistentId, out splitAt))
+            {
+                launchVehicleParts = null;
+                payloadParts = null;
+                Debug.Log(string.Format("Could not split tree, part not found {0}", separatorPart.partName));
+                return;
+            }
+
+            launchVehicleParts = new List<ProtoPartSnapshot>();
+            payloadParts = new List<ProtoPartSnapshot>();
+
+            root.AddBreadthFirst(payloadParts, splitAt);
+            splitAt.AddBreadthFirst(launchVehicleParts);
+        }
+    }
+
+    internal class ProtoPartSnapshotNode : IComparable
+    {
+        ProtoPartSnapshot part;
+        ProtoPartSnapshotNode parent;
+        List<ProtoPartSnapshotNode> children;
+
+        int IComparable.CompareTo(object obj)
+        {
+            if (obj == null)
+            {
+                return 1;
+            }
+
+            ProtoPartSnapshotNode otherNode = obj as ProtoPartSnapshotNode;
+            if (otherNode == null)
+            {
+                throw new ArgumentException("Object is not a ProtoPartSnapshotNode");
+            }
+            return part.partName.CompareTo(otherNode.part.partName);
+        }
+
+        internal ProtoPartSnapshotNode(ProtoPartSnapshot protoPart, ProtoPartSnapshotNode parentNode)
+        {
+            part = protoPart;
+            parent = parentNode;
+            children = new List<ProtoPartSnapshotNode>();
+            if (parent != null)
+            {
+                parent.AddChild(this);
+            }
+        }
+
+        internal void AddChild(ProtoPartSnapshotNode child)
+        {
+            children.Add(child);
+        }
+
+        internal void AddBreadthFirst(List<ProtoPartSnapshot> parts)
+        {
+            AddBreadthFirst(parts, null);
+        }
+
+        internal void AddBreadthFirst(List<ProtoPartSnapshot> parts, ProtoPartSnapshotNode stopAt)
+        {
+            parts.Add(part);
+            children.Sort();
+
+            foreach (ProtoPartSnapshotNode child in children)
+            {
+                if (child != stopAt)
+                {
+                    child.AddBreadthFirst(parts, stopAt);
+                }
+            }
         }
     }
 }
